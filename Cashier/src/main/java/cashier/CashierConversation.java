a10b5fcb6d7f0083a73b1bc180181b85ed093b7a
Niall <niall.p.deehan@gmail.com>
2019-10-17 15:39:54 +0200

    Removed old command line interface

--- Cashier/src/main/java/cashier/CashierConversation.java
+++ Cashier/src/main/java/cashier/CashierConversation.java
@@ -16,34 +16,34 @@ public class CashierConversation {
 
     @PostConstruct
     public void startChat() throws Exception{
-
-        Runnable newThread = () -> {
-            try {
-            Scanner scanner = new Scanner(System.in);
-
-            //new ServiceController().index(scanner.next());
-            while(true) {
-                System.out.println("Welcome to Kaffee und Kuchen, What would you like?");
-                String order = scanner.nextLine();
-                System.out.println("And what is your name?");
-                String name = scanner.nextLine();
-                try {
-                   // mike.sendOrder(order, name);
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
-
-                System.out.println("Who's next?");
-                System.out.println();
-                System.out.println();
-                System.out.println();
-            }
-
-        } catch (Exception ex) {
-            // silently ignore
-        }};
-        Thread.sleep(3000);
-        new Thread(newThread).start();
+//
+//        Runnable newThread = () -> {
+//            try {
+//            Scanner scanner = new Scanner(System.in);
+//
+//            //new ServiceController().index(scanner.next());
+//            while(true) {
+//                System.out.println("Welcome to Kaffee und Kuchen, What would you like?");
+//                String order = scanner.nextLine();
+//                System.out.println("And what is your name?");
+//                String name = scanner.nextLine();
+//                try {
+//                   // mike.sendOrder(order, name);
+//                }catch(Exception e){
+//                    e.printStackTrace();
+//                }
+//
+//                System.out.println("Who's next?");
+//                System.out.println();
+//                System.out.println();
+//                System.out.println();
+//            }
+//
+//        } catch (Exception ex) {
+//            // silently ignore
+//        }};
+//        Thread.sleep(3000);
+//        new Thread(newThread).start();
 
     }
 
